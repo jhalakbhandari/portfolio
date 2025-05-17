ba3dbc0db397847f493bb5036148b17085e81626
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-05-17 11:10:59 +0530

    Stop tracking environment folder and add to gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  googleApiKey: 'AIzaSyBQ5FPWOCw28FJsx2cgerx7TjmfLigtf3M',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  googleApiKey: 'AIzaSyBQ5FPWOCw28FJsx2cgerx7TjmfLigtf3M',
-};
